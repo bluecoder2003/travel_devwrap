622a7075a6a07b3f790208b7257ef460ec086e74
Indrani Som <indranisom39@gmail.com>
2024-03-04 07:50:59 -0800

    bug fix:minor bug fixed

--- Travel/src/components/Navbar/Navbar.js
+++ Travel/src/components/Navbar/Navbar.js
@@ -1,28 +1,48 @@
-// Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../Navbar/navbar.css';
 
 const Navbar = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <div className="nav__container">
+    <>
+      <label className="ham">
+        <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+        <span className="menu">
+          <span className="hamburger"></span>
+        </span>
+        <ul style={{ display: isChecked ? 'block' : 'none' }}>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">BOOKING</a></li>
+          <li><a href="#">TESTIMONIALS</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </label>
+      <div className="nav__container">
       <div className="nav">
         <div className='icon-container'>
-          <div className='con'><a href="#home">Home</a></div>
+          <div className='con'><a href="#">Home</a></div>
         </div>
         <div className='icon-container'>
-          <div className='con'><a href="#about">About</a></div>
+          <div className='con'><a href="#">About</a></div>
         </div>
         <div className='icon-container'>
-          <div className='con'><a href="#booking">Booking</a></div>
+          <div className='con'><a href="#">Booking</a></div>
         </div>
         <div className='icon-container'>
-          <div className='con'><a href="#testimonials">Testimonials</a></div>
+          <div className='con'><a href="#">Testimonials</a></div>
         </div>
         <div className='icon-container'>
-          <div className='con'><a href="#contact">Contact</a></div>
+          <div className='con'><a href="#">Contact </a></div>
         </div>
       </div>
     </div>
+    </>
   );
 };
 
