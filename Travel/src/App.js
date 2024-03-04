13ba72b003d35dad2e6dfb68a407ae7ceb9e8998
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2024-03-04 16:55:50 +0530

    testimonials section added

--- Travel/src/App.js
+++ Travel/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Home from "./components/Home/Home";
+import Carousel from 'react-bootstrap/Carousel';
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Booking from "./components/Booking/Booking";
-import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/Footer";
 import Destination from "./components/Destination/Destination";
+import CarouSel from "./components/Testimonials/CarouselItem";
 
 export default function App() {
   return (
@@ -16,10 +17,7 @@ export default function App() {
       <About/>
       <Booking/>
       <Destination/>
-      <div className="App">
-      <Testimonials/>
-      </div>
-      
+      <CarouSel/>
       <Contact/>
       <Footer/>
     </div>
--- Travel/src/components/Testimonials/CarouselItem.js
+++ Travel/src/components/Testimonials/CarouselItem.js
@@ -1,11 +1,51 @@
-import React from "react";
-import "../Testimonials/Testimonials.css"
-export const CarouselItem = ({ item, width }) => {
-  return (
-    <div className="carousel-item" style={{ width: width }}>
-      <div></div>
-      <img className="carousel-img" src={item.icon.default} />
-      <div className="carousel-item-text">{item.description}</div>
-    </div>
-  );
-};
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Image1 from '../../assets/Group 3.png';
+import Image2 from '../../assets/Group 4.png';
+import Image3 from '../../assets/Group 5.png';
+import './Carousel.css';
+
+function CarouSel() {
+    return (
+        <div className="carousel-container">
+            <h1 className="testimonials-heading">Testimonials</h1>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={Image1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        {/* <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={Image2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        {/* <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={Image3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        {/* <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+}
+
+export default CarouSel;
