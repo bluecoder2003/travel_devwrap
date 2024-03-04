71909ba1da72edffc998504a9f45d1fdee4f2505
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2024-03-04 17:26:53 +0530

    Logo Updated

--- Travel/src/components/Home/Home.js
+++ Travel/src/components/Home/Home.js
@@ -1,34 +1,36 @@
-import React from 'react'
-// import Navbar from "../Navbar/Navbar";
+import React from 'react';
 import { CiLocationOn } from "react-icons/ci";
 import { MdOutlineDateRange } from "react-icons/md";
 import { IoMdPerson } from "react-icons/io";
-import './Home.css'
+import './Home.css';
+import Logo from '../../assets/TRAVELLO.png'; // Import the logo image from assets folder
 
 const Home = () => {
   return (
     <div id="home" className='main__container section__padding'>
+      <img src={Logo} alt="Logo" className="logo" style={{ width: '150px', height: 'auto' }} />
+
       <p className='text glow'>
-      <span className='special-font no-glow'>Discover, Explore, Wander</span> - Your Ultimate Travel Companion!
+        <span className='special-font no-glow'>Discover, Explore, Wander</span> - Your Ultimate Travel Companion!
       </p>
       <div className='block'>
-      <div className='destination' style={{ display: 'flex', alignItems: 'center', gap: '10px', color: '#333', fontSize: '1.2rem' }}>
+        <div className='destination' style={{ display: 'flex', alignItems: 'center', gap: '10px', color: '#333', fontSize: '1.2rem' }}>
           <CiLocationOn style={{ color: ' #c22a67', fontSize: '2rem' }} /> Destination
-      </div>
-      <div className='date' style={{ display: 'flex', alignItems: 'center', gap: '10px', color: '#333', fontSize: '1.2rem' }}>
+        </div>
+        <div className='date' style={{ display: 'flex', alignItems: 'center', gap: '10px', color: '#333', fontSize: '1.2rem' }}>
           <MdOutlineDateRange style={{ color: ' #c22a67', fontSize: '2rem' }} /> Date: 01/10/2023
-      </div>
-      <div className='people' style={{ display: 'flex', alignItems: 'center', gap: '10px', color: '#333', fontSize: '1.2rem' }}>
+        </div>
+        <div className='people' style={{ display: 'flex', alignItems: 'center', gap: '10px', color: '#333', fontSize: '1.2rem' }}>
           <IoMdPerson style={{ color: ' #c22a67', fontSize: '2rem' }} /> People: 2 Adults, 1 Child
-      </div>
-      <div className='book your trip'>
-        <button className="bookTripButton">
-          Book your trip!
-        </button>
+        </div>
+        <div className='book your trip'>
+          <button className="bookTripButton">
+            Book your trip!
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
