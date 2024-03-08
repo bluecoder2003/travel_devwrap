1d35fc147f37a78c2080f9630343c6694dcbec68
Indrani Som <indranisom39@gmail.com>
2024-03-08 09:26:16 -0800

    added:signin page added

--- Travel/src/components/Home/Home.js
+++ Travel/src/components/Home/Home.js
@@ -5,8 +5,12 @@ import { MdOutlineDateRange } from "react-icons/md";
 import { IoMdPerson } from "react-icons/io";
 import './Home.css';
 import Logo from '../../assets/TRAVELLO.png'; 
-
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const navigate=useNavigate();
+  const gotosignin=()=>{
+    navigate("/signin");
+  }
   return (
     <div id="home" className='main_container section_padding'>
       <img src={Logo} alt="Logo" className="logo" style={{ width: '150px', height: 'auto' }} />
@@ -14,7 +18,7 @@ const Home = () => {
         
       <div className='auth-buttons'>
       {/* <Link to="/signin"><button className='login'>My Account</button></Link> */}
-      <button className='login'>My Account</button>
+      <button className='login'onClick={gotosignin}>My Account</button>
         {/* <button className='register'>Register</button> */}
       </div>
 
